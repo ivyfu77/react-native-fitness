4a5b0439d4c324692d82c34538f618358f06c36e
ivyfu77 <ivy@hmsnz.com>
2017-10-17 13:19:11 +1300

    Showing 5 icons with initial Slider or Stepper

--- App.js
+++ App.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
-  componentDidMount() {
-    console.log("before");
-    debugger
-    console.log("after");
-  }
   render() {
     return (
-      <View style={styles.container}>
-        <MaterialIcons name='directions-walk' color='darkgrey' size={50} />
-        <Text style={styles.test}><Ionicons name='ios-leaf' color='green' size={30} />Ivy's First React-Native app!</Text>
-        <Text style={{color: 'red'}}>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <AddEntry />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  test: {
-    fontFamily: 'Cochin',
-  }
-});
