291ec4379b7469a96baa5380aae198b3f7a64c91
ivyfu77 <ivy@hmsnz.com>
2017-12-19 16:18:12 +1300

    L5.2 Add Geolocation Live View

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
 import EntryDetail from './components/EntryDetail';
+import Live from './components/Live';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { purple, white, gray } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
@@ -35,7 +36,14 @@ const Tabs = TabNavigator({
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => (<FontAwesome name='plus-square' size={30} color={tintColor} />)
     }
-  }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => (<Ionicons name='ios-speedometer' size={30} color={tintColor} />)
+    }
+  },
 }, {
   navigationOptions: {
     header: null
